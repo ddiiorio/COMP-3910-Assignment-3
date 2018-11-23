4f62554f57f8796fba33f7636acfe9c4b6d336f4
Daniel DiIorio <ddiiorio@my.bcit.ca>
2018-11-22 22:20:08 -0800

    setting up timesheet rest uri methods

---
+++ src/com/webservice/TimesheetService.java
@@ -0,0 +1,160 @@
+package com.webservice;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import com.entity.Timesheet;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.manager.Resource;
+
+/**
+ * Defining REST URI calls for timesheet-related functionality.
+ * @author Danny
+ * @version 1.0
+ *
+ */
+@Path("/timesheets")
+public class TimesheetService {
+
+    /** Persistence entity manager object. */
+    @Inject EntityManager em;
+
+    @GET
+    @Produces("application/json")
+    public Response getTimesheets() {
+        String response = null;
+        em = Resource.getEntityManager();
+        Query query = em.createQuery("FROM com.entity.Timesheet", Timesheet.class);
+        @SuppressWarnings("unchecked")
+        List<Timesheet> list = query.getResultList();
+        em.close();
+        response = list.toString();
+        return Response.ok(response).build();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces("application/json")
+    public Response getTimesheet(@PathParam("id") int id) {
+        em = Resource.getEntityManager();
+        Timesheet timesheet = em.find(Timesheet.class, id);
+        if (timesheet == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        }
+        return Response.ok(timesheet.toString()).build();
+    }
+
+    @Transactional
+    @PUT
+    @Path("{timesheetId}")
+    @Consumes("application/json")
+    public Response updateTimesheet(@PathParam("timesheetId") int timesheetId, String payload) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+        Timesheet timesheet = gson.fromJson(payload, Timesheet.class);
+
+        System.out.println(timesheet);
+        em = Resource.getEntityManager();
+        em.getTransaction().begin();
+        Timesheet entity = em.find(Timesheet.class, timesheetId);
+        String returnCode = "";
+
+        if (entity == null)
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        try {
+            entity.setEmpNumber(timesheet.getEmpNumber());
+            entity.setEndWeek(timesheet.getEndWeek());
+            entity.setOvertime(timesheet.getOvertime());
+            entity.setFlextime(timesheet.getFlextime());
+            em.persist(entity);
+            em.flush();
+            em.getTransaction().commit();
+            em.close();
+            returnCode = "{" + "\"href\":\"http://localhost:8080/rest/timesheetservice/timesheets/" + timesheet.getTimesheetId()
+                    + "\"," + "\"message\":\"Timesheet successfully edited.\"" + "}";
+        } catch (Exception err) {
+            err.printStackTrace();
+            returnCode = "{\"status\":\"500\"," + "\"message\":\"Resource not created.\"" + "\"developerMessage\":\""
+                    + err.getMessage() + "\"" + "}";
+            return Response.status(404).entity(returnCode).build();
+
+        }
+        return Response.status(201).entity(returnCode).build();
+    }
+
+    @Transactional
+    @POST
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response createTimesheet(String payload) {
+        System.out.println("payload - " + payload);
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        Gson gson = gsonBuilder.create();
+
+        Timesheet timesheet = gson.fromJson(payload, Timesheet.class);
+        System.out.println(timesheet);
+        String returnCode = "200";
+        em = Resource.getEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            em.persist(timesheet);
+            em.flush();
+            em.refresh(timesheet);
+            em.getTransaction().commit();
+            em.close();
+
+            returnCode = "{" + "\"href\":\"http://localhost:8080/rest/timesheetservice/timesheet/" + timesheet.getTimesheetId()
+                    + "\"," + "\"message\":\"New Timesheet successfully created.\"" + "}";
+        } catch (Exception err) {
+            err.printStackTrace();
+            returnCode = "{\"status\":\"500\"," + "\"message\":\"Resource not created.\"" + "\"developerMessage\":\""
+                    + err.getMessage() + "\"" + "}";
+            return Response.status(404).entity(returnCode).build();
+
+        }
+        return Response.status(201).entity(returnCode).build();
+    }
+
+    @Transactional
+    @DELETE
+    @Consumes("application/json")
+    @Produces("application/json")
+    @Path("{timesheetId}")
+    public Response deleteTimesheet(@PathParam("timesheetId") int timesheetId) {
+        em = Resource.getEntityManager();
+        String returnCode = "";
+
+        try {
+            em.getTransaction().begin();
+            Timesheet existingTimesheet = em.find(Timesheet.class, timesheetId);
+            em.remove(existingTimesheet);
+            em.getTransaction().commit();
+            em.close();
+            returnCode = "{" + "\"message\":\"Timesheet succesfully deleted\"" + "}";
+        } catch (Exception err) {
+            err.printStackTrace();
+            returnCode = "{\"status\":\"500\"," + "\"message\":\"Resource not deleted.\"" + "\"developerMessage\":\""
+                    + err.getMessage() + "\"" + "}";
+            return Response.status(500).entity(returnCode).build();
+        }
+        return Response.ok(returnCode).build();
+    }
+}
